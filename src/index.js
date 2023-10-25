075ec87024dccc6a33c71b5db05fb5cb814c859c
Veremeev Aleksandr <a.veremeev@quantumobile.com>
2023-10-25 19:27:08 +0300

    add tls connection

--- src/index.js
+++ src/index.js
@@ -14,9 +14,18 @@ const invoiceRoute = require("./invoice/routes/invoice");
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 // CONNECTION TO DATABASE
+var uri = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}`;
+var options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}
+if (process.env.DB_TLS_ENABLED === 'true') {
+    options['tls'] = true,
+    options['tlsCAFile'] = process.env.DB_TLS_CA
+};
 mongoose.connect(
-    process.env.DB_CONNECT,
-    { useNewUrlParser: true, useUnifiedTopology: true },
+    uri,
+    options,
 );
 // MIDDLEWARE
 app.use(express.json(), cors());
